2864cefb80d6132a088bcc862a39a3d6405d9a5d
FirasCh3 <firaschebil61@gmail.com>
2025-11-19 19:12:15 +0100

    finished parser

--- src/main.c
+++ src/main.c
@@ -1,41 +1,48 @@
+#include "parser.h"
+#include "process.h"
+#include "scheduler.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "process.h"
-#include "scheduler.h"
-#include "parser.h"
 int main(void) {
-    /*ProcessList plist;
+  /*ProcessList plist;
 
-    // this will be replaced by input parsed from config
-    plist.count = 3;
-    plist.list = malloc(plist.count * sizeof(Process));
+  // this will be replaced by input parsed from config
+  plist.count = 3;
+  plist.list = malloc(plist.count * sizeof(Process));
 
-    init_process(&plist.list[0], "P1", 1, 5, 1);
-    init_process(&plist.list[1], "P2", 0, 3, 2);
-    init_process(&plist.list[2], "P3", 2, 4, 1);
-
-
-    int current_time = 0;
-    const char* policy_name = "fifo"; // this will be replaced by chosen policy from menu
-
-    while (!all_finished(&plist)) {
-        update_ready_processes(&plist, current_time);
-
-        Process* p = scheduler(plist, policy_name);
-        if (p != NULL) {
-            printf("Time %d: Running %s\n", current_time, p->name);
-            run_process_for_one_unit(p, current_time);
-        }
-        else {
-            printf("Time %d: CPU idle\n", current_time);
-        }
-
-        current_time++;
-    }
+  init_process(&plist.list[0], "P1", 1, 5, 1);
+  init_process(&plist.list[1], "P2", 0, 3, 2);
+  init_process(&plist.list[2], "P3", 2, 4, 1);
 
 
-    free(plist.list);*/
-    parse_file("config/processes.txt");
-    return 0;
+  int current_time = 0;
+  const char* policy_name = "fifo"; // this will be replaced by chosen policy
+  from menu
+
+  while (!all_finished(&plist)) {
+      update_ready_processes(&plist, current_time);
+
+      Process* p = scheduler(plist, policy_name);
+      if (p != NULL) {
+          printf("Time %d: Running %s\n", current_time, p->name);
+          run_process_for_one_unit(p, current_time);
+      }
+      else {
+          printf("Time %d: CPU idle\n", current_time);
+      }
+
+      current_time++;
+  }
+
+
+  free(plist.list);*/
+  ProcessList process_list;
+  process_list = parse_file("config/processes.txt");
+  for (int i = 0; i < process_list.count; i++) {
+    printf("%s %d %d %d\n", process_list.list[i].name,
+           process_list.list[i].arrival_time, process_list.list[i].burst_time,
+           process_list.list[i].priority);
+  }
+  return 0;
 }
--- src/parser.c
+++ src/parser.c
@@ -1,75 +1,108 @@
 #include "parser.h"
+#include "process.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
-void remove_extra_whitespaces(char* str) {
-   int i=0;
-    int j=0;
+void remove_extra_whitespaces(char *str) {
+  int i = 0;
+  int j = 0;
 
-    while (str[i]!='\0' && isspace(str[i])) {
-        i++;
+  while (str[i] != '\0' && isspace(str[i])) {
+    i++;
+  }
+  int last_char_space = 0;
+  while (str[i] != '\0') {
+    if (isspace(str[i])) {
+      if (!last_char_space) {
+        str[j] = ' ';
+        j++;
+        last_char_space = 1;
+      }
+    } else {
+      str[j] = str[i];
+      j++;
+      last_char_space = 0;
     }
-    int last_char_space = 0;
-    while (str[i] != '\0') {
-        if (isspace(str[i])) {
-            if (!last_char_space) {
-                str[j] = ' ';
-                j++;
-                last_char_space = 1;
-            }
-        }else {
-            str[j] = str[i];
-            j++;
-            last_char_space = 0;
-        }
-        i++;
-    }
-    if (j>0 && str[j-1] == ' ') {
-        j--;
-    }
-    str[j]='\0';
+    i++;
+  }
+  if (j > 0 && str[j - 1] == ' ') {
+    j--;
+  }
+  str[j] = '\0';
 }
-int find_whitespace(char* str, int start_index) {
-    int i = start_index;
-    while (str[i]!='\0' && str[i]!=' ') {
-        i++;
-    }
-    return i;
+int find_whitespace(char *str, int start_index) {
+  int i = start_index;
+  while (str[i] != '\0' && str[i] != ' ') {
+    i++;
+  }
+  return i;
 }
-ProcessList* parse_file(char* path){
-    FILE* file = fopen(path, "r");
-    char buffer[256];
-    if(file == NULL){
-        perror("Error opening file");
+
+int count_lines(char *path) {
+  char buffer[256];
+  int count = 0;
+  FILE *file = fopen(path, "r");
+  while (fgets(buffer, sizeof(buffer), file)) {
+
+    if (buffer[0] != '#' && strlen(buffer) > 1) {
+      count++;
     }
-    ProcessList* list = malloc(sizeof(ProcessList));
-    while(fgets(buffer, sizeof(buffer), file)){
+  }
+  fclose(file);
+  return count;
+}
 
-        if(buffer[0]!='#' && strlen(buffer)>1){
-            remove_extra_whitespaces(buffer);
-            Process* process = malloc(sizeof(Process));
-            int first_whitespace_index = find_whitespace(buffer, 0);
-            strncpy(process->name, buffer, first_whitespace_index);
-            int second_whitespace_index = find_whitespace(buffer, first_whitespace_index + 1);
-            char* arrival_time = (char*) malloc(second_whitespace_index - first_whitespace_index);
-            strncpy(arrival_time, buffer + first_whitespace_index + 1, second_whitespace_index - first_whitespace_index - 1);
+ProcessList parse_file(char *path) {
+  FILE *file = fopen(path, "r");
+  char buffer[256];
+  if (file == NULL) {
+    perror("Error opening file");
+  }
+  ProcessList list;
+  list.count = count_lines(path);
+  list.list = malloc(sizeof(Process) * list.count);
+  int process_index = 0;
+  rewind(file);
 
-            process->arrival_time = atoi(arrival_time);
-            printf("%s %d\n",buffer , process->arrival_time);
+  while (fgets(buffer, sizeof(buffer), file)) {
 
+    if (buffer[0] != '#' && strlen(buffer) > 1) {
+      remove_extra_whitespaces(buffer);
+      Process process;
+      int first_whitespace_index = find_whitespace(buffer, 0);
+      char *name = malloc(sizeof(char) * 32);
+      strncpy(name, buffer, first_whitespace_index);
+      name[first_whitespace_index] = '\0';
+      int second_whitespace_index =
+          find_whitespace(buffer, first_whitespace_index + 1);
+      char *arrival_time =
+          (char *)malloc(second_whitespace_index - first_whitespace_index);
+      strncpy(arrival_time, buffer + first_whitespace_index + 1,
+              second_whitespace_index - first_whitespace_index - 1);
+      int third_whitespace =
+          find_whitespace(buffer, second_whitespace_index + 1);
+      char *burst_time =
+          (char *)malloc(third_whitespace - second_whitespace_index);
+      strncpy(burst_time, buffer + second_whitespace_index + 1,
+              third_whitespace - second_whitespace_index - 1);
+      int fourth_whitespace = find_whitespace(buffer, third_whitespace + 1);
+      char *priority = (char *)malloc(fourth_whitespace - third_whitespace);
 
-            /*char* burst_time = "";
-            strncpy(burst_time, buffer+3, 1);
-            process->burst_time = atoi(burst_time);
-            char* priority = "";
-            strncpy(priority, buffer+4, 1);
-            process->priority = atoi(priority);*/
-        }
+      strncpy(priority, buffer + third_whitespace + 1,
+              fourth_whitespace - third_whitespace - 1);
+
+      init_process(&process, name, atoi(arrival_time), atoi(burst_time),
+                   atoi(priority));
+      list.list[process_index] = process;
+      free(name);
+      free(arrival_time);
+      free(burst_time);
+      free(priority);
+      process_index++;
     }
-    fclose(file);
-    return list;
-
-
+  }
+  fclose(file);
+  return list;
 }
